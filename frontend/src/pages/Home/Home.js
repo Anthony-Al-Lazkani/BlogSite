79f02ef502cb87d3091a7cbab075067bcc3ae30e
Thalia Atallah <thaliaatallah8@gmail.com>
2024-06-27 14:59:38 +0300

    Started home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom"
+import blogImage from '../../images/blog.png' 
+import './Home.css'
+import Image from '../../images/blog.png'
 
 const Home = () => {
     return(
         <div className="home">
-            <h2>HOME</h2>
+            <div className="image-container">
+                <h1>heheheheeheh</h1>
+                
+            </div>
 
             
         </div>
