ae68931c7580810f3e6ec2bd27f314ee619c38ea
Anthony Al Lazkani <anthonylazkani.22@gmail.com>
2024-06-27 16:22:44 +0300

    Started the Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // Import Pages and Components
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
+import Blogs from './pages/Blogs/Blogs';
+import ContactUs from './pages/ContactUS/ContactUs';
+import Login from './pages/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
@@ -14,7 +16,9 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/About' element={<About />} />
+            <Route path='/Blogs' element={<Blogs />} />
+            <Route path='/ContactUs' element={<ContactUs />} />
+            <Route path='/Login' element={<Login />} />
           </Routes>
         </div>
         <Footer />
---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Login.css"
+
+
+function Login() {
+    return(
+        <div className="Container">
+            <div className="LoginContainer">
+                <div className="title">
+                    <h1>Login</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
