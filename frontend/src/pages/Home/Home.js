033b61a66ddd05d4ffa532c6c3f693f5232bd3b2
Thalia Atallah <thaliaatallah8@gmail.com>
2024-06-28 15:50:07 +0300

    Started Footer

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,16 +1,9 @@
 import React from "react";
-import './Home.css'
+import './Home.css';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
-import Photo1 from '../../images/hi3.jpg';
-import Photo2 from '../../images/hi4.jpeg';
-import Photo3 from '../../images/hi5.jpg';
-// import Photo4 from '../../images/pic4.jpg';
-// import Photo5 from '../../images/photo5.jpg';
-
+import Photo1 from '../../Images/hi3.png';
 
 function Home() {
     const history = useNavigate();
@@ -19,33 +12,15 @@ function Home() {
     };
     const goToLogin = () => {
         history('/login');
-    }
-
-    const images = [Photo1, Photo2, Photo3];
-    //Photo2, Photo3, Photo4, Photo5
-
-
-    const properties = {
-        prevArrow: <div style={{ visibility: 'hidden' }}></div>,
-        nextArrow: <div style={{ visibility: 'hidden' }}></div>,
-        autoplay: true,
-        duration: 5000,
-        transitionDuration: 2000,
-        onChange: () => { }, // Your onChange function
-        onStartChange: () => { } // Your onStartChange function
     };
 
     return (
         <>
             <section className="Front-Page">
-                <Slide {...properties} >
-                    {images.map((image, index) => (
-                        <div key={index} className="each-slide-effect">
-                            <div style={{ 'backgroundImage': `url(${image})` }}>
-                            </div>
-                        </div>
-                    ))}
-                </Slide>
+                <div className="each-slide-effect">
+                    <div style={{ 'backgroundImage': `url(${Photo1})` }}>
+                    </div>
+                </div>
                 <div className="Front-Page-Content">
                     <h1 className="Font-Page-Header-Text">Your Story. Your Community. Your Voice.</h1>
                     <p>Join a vibrant community where your stories come to life. Explore diverse perspectives, engage in meaningful discussions, and share your voice with the world.
@@ -59,7 +34,6 @@ function Home() {
 
             <section className="Services">
                 <h1>Services We Offer</h1>
-
                 <div className="Row">
                     <div className="Services-Col">
                         <h3>Create a blog</h3>
@@ -78,38 +52,11 @@ function Home() {
                 </div>
             </section>
 
-            {/* <section className="Facilities">
-                <h1>What you can find?</h1>
-
-                <div className="Row">
-                    <div className="Facilities-Col">
-                        <div className="Food-Image"></div>
-                        <h3>Top Tier Food</h3>
-                        <p>Restaurants with the best food that you can find in our beloved country! </p>
-                    </div>
-                    <div className="Facilities-Col">
-                        <div className="Hotel-Image"></div>
-                        <h3>Most Luxirious Hotels</h3>
-                        <p>
-                            Hotels in Lebanon offer a unique blend of Middle Eastern hospitality, combining modern amenities with a rich cultural experience </p>
-                    </div>
-                    <div className="Facilities-Col">
-                        <div className="Beach-Image"></div>
-                        <h3>Beaches In Lebanon</h3>
-                        <p>Lebanon's beaches beckon with their Mediterranean charm, boasting crystal-clear waters, golden sands, and vibrant seaside culture, creating an idyllic coastal escape.</p>
-                    </div>
-                </div>
-            </section> */}
-
-
             <section className="cta">
                 <h1>Have questions, suggestions, or feedback? We'd love to hear from you! <br />Feel free to reach out to our team by pressing the button below !</h1>
                 <Link to="/ContactUs"><button>CONTACT US</button></Link>
             </section>
-
-
         </>
-
     );
 }
 
