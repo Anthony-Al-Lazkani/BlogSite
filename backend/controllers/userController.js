e591fa88ff5ffda48f3707f3bc72ea90c3fb5050
KarimHabbal1 <kmhabbal@gmail.com>
2024-06-30 21:24:39 +0300

    added token authentication function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,6 +39,28 @@ function decodeToken(token) {
     }
 }
 
+//middleware to authenticate the token
+function authenticateToken(req, res, next) {
+    //extract token
+    const token = extractAuthToken(req);
+
+    //if no token found respond with 403 
+    if (!token) {
+        return res.sendStatus(403);
+    }
+
+    // Try to decode the token
+    try {
+        const userId = decodeToken(token);
+        //if token is valid attach it to req object so we can use the id in the functions
+        req.userId = userId;
+        next(); 
+    } catch (error) {
+        // If token is invalid or expired, respond with 403 (Forbidden)
+        return res.sendStatus(403);
+    }
+}
+
 //CreateUser
 const createUser = async (req, res) => {
     try{
