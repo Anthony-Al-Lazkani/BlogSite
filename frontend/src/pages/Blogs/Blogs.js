116a7ab3688967c41b5fd1d37ce72866922622b1
Anthony Al Lazkani <anthonylazkani.22@gmail.com>
2024-07-01 10:08:02 +0300

    Fixed the Blogs

--- frontend/src/pages/Blogs/Blogs.js
+++ frontend/src/pages/Blogs/Blogs.js
@@ -1,11 +1,26 @@
 import React from "react";
 import './Blogs.css'
+import Card from 'react-bootstrap/Card';
+
 
 function Blogs() {
     return(
         <>
             <section className="First-Page">
                 <h1>Enjoy Our Collection Of Latest Blogs !</h1>
+
+                <div className="Blogs">
+                    <Card>
+                        <Card.Body className="Blog-TXT">
+                        <Card.Title>Blog</Card.Title>
+                        <Card.Text>
+                           Hello World
+                        </Card.Text>
+                        <Card.Link>Read More</Card.Link>
+                        </Card.Body>
+                    </Card>
+
+                </div>
                 
             </section>
         </>
