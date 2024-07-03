783150817482599686381cac58c7df7af4c0644d
Thalia Atallah <thaliaatallah8@gmail.com>
2024-07-03 08:46:37 +0300

    working on reviews

---
+++ frontend/src/pages/Home/review1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './review.css';
+
+function Review1(){
+    return (
+    <div className="wrapper-1"> 
+        <h1>Eva Sawyers</h1>
+        <h2>CEO, Fashworks</h2>
+        <p>Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur</p>
+    </div>
+    );
+}; 
+
+export default Review1;
---
+++ frontend/src/pages/Home/review2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './review.css';
+
+function Review2() {
+    return (
+        <div className="wrapper-1"> 
+            <h1>Katey Topaz</h1>
+            <h2>Developer, TechCrew</h2>
+            <p>Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur</p>
+        </div>
+    );
+}
+
+export default Review2; 
