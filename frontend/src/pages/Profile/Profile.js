6e995a92d5e2ed65008a46dcdcb6ad9ccb20c555
wajed <wajed972@outlook.com>
2024-07-04 11:22:00 +0300

    added notification where friend request appears and implemented it

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -5,8 +5,9 @@ import './Profile.css';
 function Profile() {
     const username = localStorage.getItem('username');
     const email = localStorage.getItem('email');
-    const friends = localStorage.getItem('friends');
-    const navigate = useNavigate()
+    const friendsString = localStorage.getItem('friends');
+    const friends = JSON.parse(friendsString);
+    const navigate = useNavigate();
 
     const onClose = () => {
         navigate('/');
@@ -19,7 +20,16 @@ function Profile() {
                 <h2>Profile Information</h2>
                 <p><strong>Username:</strong> {username || 'N/A'}</p>
                 <p><strong>Email:</strong> {email || 'N/A'}</p>
-                <p><strong>Friends:</strong> {friends || 'No friends available'}</p>
+                <p><strong>Friends:</strong></p>
+                {friends && friends.length > 0 ? (
+                    <ul>
+                        {friends.map((friend, index) => (
+                            <li key={index}>{friend.username}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>No friends available</p>
+                )}
             </div>
         </div>
     );
