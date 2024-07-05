5382950a55183aab424007133abca4118e01e5da
wajed <wajed972@outlook.com>
2024-07-05 09:29:06 +0300

    search engine for blogs

--- frontend/src/pages/Blogs/Blogs.js
+++ frontend/src/pages/Blogs/Blogs.js
@@ -1,98 +1,84 @@
-import React from "react";
-import './Blogs.css'
+import React, { useEffect, useState } from "react";
+import './Blogs.css';
 import Card from 'react-bootstrap/Card';
-import { useEffect, useState } from "react";
 import BlogForm from "../../components/BlogForm/BlogForm";
-import '@coreui/coreui/dist/css/coreui.min.css'
-import { FaHome } from "react-icons/fa";
+import '@coreui/coreui/dist/css/coreui.min.css';
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { FaPlus, FaMinus  } from "react-icons/fa";
+import { FaPlus, FaMinus } from "react-icons/fa";
 import SubmissionForm from "../../components/SubmissionForm/SubmissionForm";
 import { useArticlesContext } from "../../hooks/useArticlesContext";
 import axios from "axios";
 
-
-
-
-
-
-
-
 function Blogs() {
-
-    // const [articles, setArticles] = useState(null)
-    const [form, setForm] = useState(false)
-    const [icon, setIcon] = useState(false)
-    const { articles, dispatch } = useArticlesContext()
+    const [form, setForm] = useState(false);
+    const [icon, setIcon] = useState(false);
+    const { articles, dispatch } = useArticlesContext();
     const isLoggedIn = !!localStorage.getItem('authToken');
-    const token = localStorage.getItem('authToken')
-    console.log(token)
-  
-
-
+    const token = localStorage.getItem('authToken');
+    const [search, setSearch] = useState(""); // State for search input
 
     const toggleForm = () => {
-        if (!isLoggedIn){
-            alert("You need to Sign In First !")
-        }else {
+        if (!isLoggedIn) {
+            alert("You need to Sign In First !");
+        } else {
             setForm(!form);
             setIcon(!icon);
         }
     }
 
-
-
-
-
-    useEffect(()=>{
+    useEffect(() => {
         const fetchArticle = async () => {
-            const response = await axios.get('/api/articles/getArticlesSortedByTime')
-            // const json = await response.json()
-            dispatch({type: 'SET_ARTICLES', payload:response.data})
-
-            // if (response.ok) {
-            //     setArticles(json)
-            // }
+            const response = await axios.get('/api/articles/getArticlesSortedByTime');
+            dispatch({ type: 'SET_ARTICLES', payload: response.data });
         }
+        fetchArticle();
+    }, [dispatch]);
 
-        fetchArticle()
-    }, [])
+    // Filter articles based on search input
+    const filteredArticles = articles.filter(article => 
+        article.title.toLowerCase().includes(search.toLowerCase()) || 
+        article.content.toLowerCase().includes(search.toLowerCase())
+    );
 
-    return(
-            <div className="Home-Page">
-
-                <div className="First-Part">
-                    <Sidebar />
+    return (
+        <div className="Home-Page">
+            <div className="First-Part">
+                <Sidebar />
+            </div>
+            <div className="Second-Part">
+                <input 
+                    type="text" 
+                    placeholder="Search articles..." 
+                    value={search} 
+                    onChange={(e) => setSearch(e.target.value)} 
+                    className="search-bar"
+                />
+                <div className="articles">
+                    {filteredArticles && filteredArticles.map((article) => (
+                        <BlogForm key={article._id} article={article} />
+                    ))}
                 </div>
-                <div className="Second-Part">
-                    <div className="articles">
-                        {articles && articles.map((article) => (
-                            <BlogForm key={article._id} article={article} />
-                        ))}
-                    </div>
-                </div>
-                <div className="Third-Part">
-                    <div className="PlusIconDiv">
-                        <div className="PlusIcon" onClick={toggleForm}>
-                            {icon  ? (
-                                <FaMinus />
-                            ) : (
-                               <FaPlus /> 
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="NewArticleForm">
-                        {form ? (
-                                  <SubmissionForm />
-                                ) : (
-                                    <div></div>
+            </div>
+            <div className="Third-Part">
+                <div className="PlusIconDiv">
+                    <div className="PlusIcon" onClick={toggleForm}>
+                        {icon ? (
+                            <FaMinus />
+                        ) : (
+                            <FaPlus />
                         )}
                     </div>
                 </div>
+                <div className="NewArticleForm">
+                    {form ? (
+                        <SubmissionForm />
+                    ) : (
+                        <div></div>
+                    )}
+                </div>
             </div>
+        </div>
     )
 }
 
-
-export default Blogs
+export default Blogs;
